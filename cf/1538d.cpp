453e4b2d6f168094f3481a17651b3738ce2854ae
ProudCarrot <1419391645@qq.com>
2023-09-12 20:56:24 +0800

    .

--- cf/1538d.cpp
+++ cf/1538d.cpp
@@ -15,82 +15,37 @@ using namespace std;
 #define fg
 #endif
 
-vector<int> prime;
-
-void get_prime() {
-    const int N = 1e5;
-    map<int, int> vis;
-    for (int i = 2; i <= N; i++) {
-        if (vis.count(i) == 0) {
-            prime.push_back(i);
-            // cout<<i<<' ';
-        }
-
-        for (int j = 0; j < prime.size(); j++) {
-            if (i * prime[j] > N)
-                break;
-            vis[i * prime[j]] = 1;
-            if (i % prime[j] == 0)
-                break;
-        }
-    }
-}
-
-void solve1() {
-    int a, b, k;
-    cin >> a >> b >> k;
-    multiset<int> st;
-    for (int i = 2; i <= a; i++) {
-        while (a % i == 0) {
-            st.insert(i);
-            a /= i;
-        }
-    }
-    for (auto i : st)
-        cout << i << ' ';
-    cout << endl;
-
-    st.clear();
-    for (int i = 2; i <= b; i++) {
-        while (b % i == 0) {
-            st.insert(i);
-            b /= i;
-        }
-    }
-    for (auto i : st)
-        cout << i << ' ';
-    cout << endl;
-    cout << "---------------" << endl;
-}
-
 void solve() {
     int a, b, k;
     cin >> a >> b >> k;
-
-    vector<int> ys1, ys2;
-    for (int i = 0; i < prime.size() && prime[i] * prime[i] <= a; i++) {
-        while (a % prime[i] == 0) {
-            ys1.push_back(prime[i]);
-            a /= prime[i];
-        }
+    if (k == 1) {
+        if ((a % b == 0 || b % a == 0) && a != b)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+        return;
     }
 
     int res = 0;
-    for (int i = 0; i < ys1.size(); i++) {
-        if (b % ys1[i] == 0)
-            b /= ys1[i];
-        else
+    for (int i = 2; i * i <= a; i++) {
+        while (a % i == 0) {
             res++;
+            a /= i;
+        }
     }
-
-    for (int i = 0; i < prime.size() && prime[i] * prime[i] <= b; i++) {
-        while (b % prime[i] == 0) {
+    for (int i = 2; i * i <= b; i++) {
+        while (b % i == 0) {
             res++;
-            b /= prime[i];
+            b /= i;
         }
     }
 
-    cout << res << endl;
+    // cout<<res<<endl;
+
+    if (res > k)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
 }
 
 signed main() {
@@ -102,12 +57,10 @@ signed main() {
     cout.tie(0);
 #endif
 
-    get_prime();
     int tt = 1;
     cin >> tt;
-    // cout<<tt<<endl;
     while (tt--)
-        solve1();
+        solve();
 
     return 0;
 }
