5fea4d7fdd4b2d3b36749b2fce718dd3261cebcb
ProudCarrot <1419391645@qq.com>
2023-10-20 21:10:19 +0800

    "nan ning"

--- cf/tmp.cpp
+++ cf/tmp.cpp
@@ -1,61 +1,53 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define int int64_t
-typedef pair<int, int> pii;
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n), b(n), c(1 >> 8, 0), t(1 >> 8, 0);
-    for (int& i : a)
-        cin >> i;
-    int ans = 0;
-    function<vector<int>(int)> itoss = [&](int x) {
-        vector<int> tp(8);
-        for (int i = 0; i < 8; i++)
-            tp[i] = x >> (7 - i) & 1;
-        return tp;
-    };
-    function<int(int)> query = [&](int x) {
-        cout << "x : " << x << '\n';
-        auto s = itoss(x);
-        int p = 1;
-        for (int i : s) {
-            if (c[p * 2 + (!i)])
-                p = p * 2 + (!i);
-            else if (c[p * 2 + i])
-                p = p * 2 + i;
-            else
-                break;
-        }
-        return t[p] ^ x;
-    };
+#define ll long long
+#define endl "\n"
 
-    function<void(int)> insert = [&](int x) {
-        auto s = itoss(x);
-        int p = 1;
-        for (int i : s) {
-            if (!c[p * 2 + i])
-                c[p * 2 + i] = 1;
-            p = p * 2 + i;
-        }
-        t[p] = x;
-    };
-    // int pp = query(10);
-    for (int i = 0; i < n; i++) {
-        if (i == 0)
-            b[i] = a[i];
-        else
-            b[i] = b[i - 1] ^ a[i];
-        ans = max(ans, query(a[i]));
-        // cout << i + 1 << " : "<< ans << '\n' ;
-        insert(b[i]);
-    }
-    cout << ans << '\n';
+using namespace std;
+
+const int N = 1000 + 5;
+const double INF = LLONG_MAX / 2;
+double x[N], y[N], r[N];
+double p[N];
+
+double dp[N][N];
+
+double D(double x1, double y1, double x2, double y2) {
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
-signed main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int _; cin >> _;
-    while (_ -- )
-    solve();
-    return 0;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++)
+        scanf("%lf %lf %lf %lf", &x[i], &y[i], &r[i], &p[i]);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j)
+                dp[i][j] = 0;
+            else if (D(x[i], y[i], x[j], y[j]) <= r[i] * r[i])
+                dp[i][j] = p[i] * r[i];
+            else
+                dp[i][j] = INF;
+        }
+    }
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+            }
+        }
+    }
+
+    int a, b;
+    while (m--) {
+        cin >> a >> b;
+
+        if (dp[a][b] != INF)
+            printf("%.6lf\n", dp[a][b]);
+        else
+            printf("-1\n");
+    }
 }
